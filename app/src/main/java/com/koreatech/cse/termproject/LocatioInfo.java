d8e5c7a2c143dcf3243789a86f4f243eb9f7aca5
there9 <there924@naver.com>
2016-05-27 21:29:18 +0900

    logRemainVersion
    logtext correct and LocationInfo add

---
+++ app/src/main/java/com/koreatech/cse/termproject/LocatioInfo.java
@@ -0,0 +1,36 @@
+package com.example.user.myapplication;
+
+/**
+ * Created by HOME on 2016-05-27.
+ */
+public class LocatioInfo {
+    String locationName;
+    float time;
+    int step;
+    static int totalStepCount;
+    static int totalMovingTime;
+    LocatioInfo(String locationName, int step, float time)
+    {
+        this.locationName = locationName;
+        this.step = step;
+        this.time = time;
+    }
+    public float getTime() {
+        return time;
+    }
+
+    public int getStep() {
+        return step;
+    }
+    public String getLocationName() {
+        return locationName;
+    }
+    public int totalSumStep(int step)
+    {
+        return totalStepCount += step;
+    }
+    public int totalSumMovingTime(int movingTime)
+    {
+        return totalMovingTime += movingTime;
+    }
+}
